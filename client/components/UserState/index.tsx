96adecacb33b27f4c999fc53caa7aaaf674f96e9
dolphinlmg <dolphinlmg@naver.com>
2023-04-01 22:54:58 +0900

    ✨ feat: UserState 컴포넌트 작성
    Close #17

---
+++ client/components/UserState/index.tsx
@@ -0,0 +1,40 @@
+import { css } from '@emotion/react';
+import Button from '../Button';
+
+const style = {
+  container: css`
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    width: 240px;
+    justify-content: space-around;
+  `,
+  a: css`
+    text-decoration-line: none;
+    color: #747474;
+    font-size: 20px;
+  `,
+};
+
+interface UserStateProps {
+  isLoggedIn: boolean;
+}
+
+export default function UserState({ isLoggedIn }: UserStateProps) {
+  const signUpUrl = '';
+  const myPageUrl = '';
+
+  return (
+    <div css={style.container}>
+      <a href={isLoggedIn ? myPageUrl : signUpUrl} css={style.a}>
+        {isLoggedIn ? '마이페이지' : '회원가입'}
+      </a>
+      <Button
+        value={isLoggedIn ? '로그아웃' : '로그인'}
+        width={'93px'}
+        height={'48px'}
+        fontSize={'20px'}
+      />
+    </div>
+  );
+}
