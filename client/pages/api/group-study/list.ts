3dea971ed3ede1dd5f6a522855046b1f832cab88
dolphinlmg <dolphinlmg@naver.com>
2023-04-05 16:50:18 +0900

    ✨ feat: GroupStudyList mock API 기본 응답 개수 증가

--- client/pages/api/group-study/list.ts
+++ client/pages/api/group-study/list.ts
@@ -27,6 +27,32 @@ const list: GroupStudyListResponse = [
     ownerId: 'asdfsadf',
     isFinished: false,
   },
+  {
+    state: 'ongoing',
+    groupId: 'test',
+    languageId: 'qwer',
+    groupName: 'groupName',
+    groupPersonnel: 3,
+    tags: ['# 태그1', '# 태그2'],
+    introduction:
+      '안녕하세요. 개인적으로 네명정도 토익 스피킹 스터디 진행하실 분 모십니다. 매주 2일 스터디를 진행할 예정이며 현재 2명 모집되었습니다. 앞으로 두...',
+    groupDuration: new Date(),
+    ownerId: 'asdfsadf',
+    isFinished: false,
+  },
+  {
+    state: 'ongoing',
+    groupId: 'test',
+    languageId: 'qwer',
+    groupName: 'groupName',
+    groupPersonnel: 3,
+    tags: ['# 태그1', '# 태그2'],
+    introduction:
+      '안녕하세요. 개인적으로 네명정도 토익 스피킹 스터디 진행하실 분 모십니다. 매주 2일 스터디를 진행할 예정이며 현재 2명 모집되었습니다. 앞으로 두...',
+    groupDuration: new Date(),
+    ownerId: 'asdfsadf',
+    isFinished: false,
+  },
 ];
 
 export default function handler(req: NextApiRequest, res: NextApiResponse<GroupStudyListResponse>) {
