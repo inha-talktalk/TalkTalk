b68fe0b62539bd7b3e9b7f43c1179619ee9d0624
dolphinlmg <dolphinlmg@naver.com>
2023-04-05 16:52:55 +0900

    ✨ feat: index 페이지 작성
    - 모든 이벤트 설정 필요
      - 스터디 게시글 클릭
      - 회원가입/로그인
      - Footer
    
    Related to: #29

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -1,3 +1,97 @@
+import Footer from '@/components/Footer';
+import NavBar from '@/components/NavBar';
+import PostCell from '@/components/PostCell';
+import { getGroupStudyList, getUserProfile } from '@/utils/api';
+import { css } from '@emotion/react';
+import Image from 'next/image';
+import { useEffect, useState } from 'react';
 export default function Home() {
-  return <div>Hello world!</div>;
+  const style = {
+    nav: css`
+      position: fixed;
+      top: 0;
+      left: 0;
+      width: 100%;
+      z-index: 100;
+    `,
+    mainImage: css`
+      margin-top: 94px;
+      width: 100%;
+      height: 500px;
+      position: relative;
+      user-select: none;
+    `,
+    groupStudyListContainer: css`
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      margin: 50px 0 150px 0;
+
+      & > p {
+        font-size: 30px;
+        width: 1010px;
+      }
+    `,
+  };
+
+  const [groupStudyList, setGroupStudyList] = useState<GroupStudy[]>([]);
+  const [groupStudyOwnerList, setGroupStudyOwnerList] = useState<UserProfile[]>([]);
+
+  // to GET Group Study List
+  useEffect(() => {
+    (async () => {
+      try {
+        const list = await getGroupStudyList();
+        setGroupStudyList(list);
+      } catch (e) {
+        if (e instanceof Error) {
+          console.error(`Error by getGroupStudyList() on index\n${e.message}`);
+        }
+      }
+    })();
+  }, []);
+
+  // to GET Group Study Owner Profile
+  useEffect(() => {
+    (async () => {
+      try {
+        const getUserPromises = groupStudyList.map((groupStudy) =>
+          getUserProfile(groupStudy.ownerId),
+        );
+
+        setGroupStudyOwnerList(await Promise.all(getUserPromises));
+      } catch (e) {
+        if (e instanceof Error) {
+          console.error(`Error by getGroupStudyList() on index\n${e.message}`);
+        }
+      }
+    })();
+  }, [groupStudyList]);
+
+  return (
+    <>
+      <div css={style.nav}>
+        <NavBar />
+      </div>
+      <div css={style.mainImage}>
+        <Image src={'/main.png'} alt={'main'} fill={true} />
+      </div>
+      <div css={style.groupStudyListContainer}>
+        <p>최근 스터디 모집</p>
+        {groupStudyList.length === groupStudyOwnerList.length &&
+          Array(groupStudyList.length)
+            .fill(0)
+            .map((_, i) => i)
+            .map((i) => (
+              <PostCell
+                group={groupStudyList[i]}
+                owner={groupStudyOwnerList[i]}
+                isLast={i === groupStudyList.length - 1}
+                key={groupStudyList[i].groupId}
+              />
+            ))}
+      </div>
+      <Footer />
+    </>
+  );
 }
