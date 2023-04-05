30db8b8b60f4e18f89f0b097da6d8be7373194ba
dolphinlmg <dolphinlmg@naver.com>
2023-04-05 16:00:52 +0900

    🐛 fix: api path 수정

---
+++ client/utils/api.ts
@@ -0,0 +1,13 @@
+import axios from 'axios';
+
+const BASE_URL = process.env.NEXT_PUBLIC_API_PATH;
+
+if (!BASE_URL) throw new Error('API_BASE_URL이 없습니다.');
+
+export const apiAxiosInstance = axios.create({
+  baseURL: BASE_URL,
+});
+
+export async function getUserProfile(userId: string) {
+  return (await apiAxiosInstance.get(`/user/profile/${userId}`)).data;
+}
