1207207ddc42895eafc74e3a0ff7ec3849198af0
dolphinlmg <dolphinlmg@naver.com>
2023-04-07 16:12:18 +0900

    feat: placeholder, Lazy Image 구현
    작은 사이즈의 회색 placeholder 추가
    
    Related to: #48

---
+++ client/components/LazyImage/index.tsx
@@ -0,0 +1,26 @@
+import { DEAFULT_PLACEHOLDER_GRAY } from '@/utils/image';
+import { SerializedStyles } from '@emotion/react';
+import Image from 'next/image';
+
+interface LazyImageProps {
+  src: string;
+  width?: number;
+  height?: number;
+  alt: string;
+  innerCss?: SerializedStyles;
+}
+
+export default function LazyImage({ src, width, height, alt, innerCss }: LazyImageProps) {
+  return (
+    <Image
+      src={src}
+      width={width}
+      height={height}
+      alt={alt}
+      css={innerCss}
+      placeholder="blur"
+      blurDataURL={DEAFULT_PLACEHOLDER_GRAY}
+      loading="lazy"
+    />
+  );
+}
