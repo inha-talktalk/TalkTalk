30aac727b146871bb6ea7d6c27256efb481fe109
Young998904 <like0998904@gmail.com>
2023-04-22 14:24:05 +0900

    :truck: OAuthUserDto의 nickname 을 name 으로 변경

--- server/src/main/java/com/inha/server/oauth/dto/OAuthUserDto.java
+++ server/src/main/java/com/inha/server/oauth/dto/OAuthUserDto.java
@@ -8,7 +8,7 @@ import lombok.Getter;
 public class OAuthUserDto {
 
   private Long id;
-  private String nickname;
+  private String name;
   private String email;
   private String profileImage;
 }
