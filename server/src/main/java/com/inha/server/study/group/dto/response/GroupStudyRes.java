bb6ccca2316f801025e21f3a43dbf914a4164cd3
jiwon1217 <koagjw@gmail.com>
2023-04-24 01:50:51 +0900

    :necktie: feat: 스터디 그룹 게시글 생성, 스터디 그룹 게시글 목록 조회 response, request 추가

---
+++ server/src/main/java/com/inha/server/study/group/dto/response/GroupStudyRes.java
@@ -0,0 +1,24 @@
+package com.inha.server.study.group.dto.response;
+
+import java.util.List;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class GroupStudyRes {
+
+  private String groupId;
+  private String languageId;
+  private String groupName;
+  private Long groupPersonnel;
+  private List<String> tags;
+  private String introduction;
+  private String groupDuration;
+  private String ownerId;
+  private Boolean isFinished;
+}
