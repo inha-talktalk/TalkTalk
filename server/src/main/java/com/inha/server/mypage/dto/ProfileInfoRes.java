cebeadf494f3a270c12330fe921a64ac388bfc2b
Young998904 <like0998904@gmail.com>
2023-04-24 06:28:35 +0900

    :necktie: feat : 마이페이지 조회 dto 추가

---
+++ server/src/main/java/com/inha/server/mypage/dto/ProfileInfoRes.java
@@ -0,0 +1,11 @@
+package com.inha.server.mypage.dto;
+
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
+public class ProfileInfoRes {
+    private String userName;
+    private String nickName;
+    private String email;
+    private String profileIconUrl;
+}
