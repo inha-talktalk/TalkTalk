6701601a0efc62ba36d520bb883cc9daa7bd8610
Young998904 <like0998904@gmail.com>
2023-04-24 17:31:50 +0900

    :zap: fix : ProfileInfoRes에 Builder 적용

--- server/src/main/java/com/inha/server/mypage/dto/ProfileInfoRes.java
+++ server/src/main/java/com/inha/server/mypage/dto/ProfileInfoRes.java
@@ -1,9 +1,15 @@
 package com.inha.server.mypage.dto;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
 
+@Builder
+
+@Getter
 @AllArgsConstructor
 public class ProfileInfoRes {
+
     private String userName;
     private String nickName;
     private String email;
