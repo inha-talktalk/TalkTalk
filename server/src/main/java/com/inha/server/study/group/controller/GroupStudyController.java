acb21c55a2fc60e6ceeb5c8411ea97cf7c744bff
jiwon1217 <koagjw@gmail.com>
2023-04-26 01:25:14 +0900

    :sparkles: feat: 스터디 종료 controller

--- server/src/main/java/com/inha/server/study/group/controller/GroupStudyController.java
+++ server/src/main/java/com/inha/server/study/group/controller/GroupStudyController.java
@@ -1,6 +1,7 @@
 package com.inha.server.study.group.controller;
 
 import com.inha.server.study.group.dto.request.PostGroupStudyReq;
+import com.inha.server.study.group.dto.response.DeleteGroupStudyRes;
 import com.inha.server.study.group.dto.response.GetGroupStudyDetailRes;
 import com.inha.server.study.group.dto.response.GetGroupStudyListRes;
 import com.inha.server.study.group.dto.response.PostGroupStudyRes;
@@ -9,6 +10,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort.Direction;
 import org.springframework.data.web.PageableDefault;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -40,5 +42,11 @@ public class GroupStudyController {
   public GetGroupStudyDetailRes read(@RequestHeader(value = "x-access-token") String jwt,
       @RequestParam(value = "groupStudyId") String groupStudyId) {
     return groupStudyService.getGroupStudyDetail(groupStudyId);
+  }
+
+  @DeleteMapping("")
+  public DeleteGroupStudyRes delete(@RequestHeader(value = "x-access-token") String jwt,
+      @RequestParam(value = "groupStudyId") String groupStudyId) {
+    return groupStudyService.delete(jwt, groupStudyId);
   }
 }
