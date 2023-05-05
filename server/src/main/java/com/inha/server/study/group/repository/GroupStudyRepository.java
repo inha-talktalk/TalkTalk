57f3efd8de1abc9fd55d67797bbf7715e9c98d8c
jiwon1217 <koagjw@gmail.com>
2023-05-06 04:41:37 +0900

    :sparkles: feat: 그룹 스터디 검색 repository method 추가

--- server/src/main/java/com/inha/server/study/group/repository/GroupStudyRepository.java
+++ server/src/main/java/com/inha/server/study/group/repository/GroupStudyRepository.java
@@ -1,6 +1,7 @@
 package com.inha.server.study.group.repository;
 
 import com.inha.server.study.group.model.GroupStudy;
+import java.util.List;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.mongodb.repository.MongoRepository;
@@ -11,4 +12,8 @@ public interface GroupStudyRepository extends MongoRepository<GroupStudy, String
 
   @Override
   Page<GroupStudy> findAll(Pageable pageable);
+
+  List<GroupStudy> findByIntroductionContainingIgnoreCase(String keyword);
+
+  List<GroupStudy> findByIntroductionContainingIgnoreCase(String keyword, Pageable pageable);
 }
