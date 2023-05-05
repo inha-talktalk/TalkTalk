05fccee4cc539f8ed3c3dbe472c132cdfe6bb66c
jiwon1217 <koagjw@gmail.com>
2023-05-06 03:13:11 +0900

    :necktie: feat: 스터디 탈퇴 response 작성

---
+++ server/src/main/java/com/inha/server/study/group/dto/response/PostGroupStudyQuitRes.java
@@ -0,0 +1,15 @@
+package com.inha.server.study.group.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class PostGroupStudyQuitRes {
+
+  private String quitUserId;
+}
