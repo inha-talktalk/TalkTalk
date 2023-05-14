82fdfa54319cfb534b257c76da159f9fc7655be5
Cho Ah Young <like0998904@gmail.com>
2023-05-14 22:25:49 +0900

    :sparkles: feat : clova voice 기능 구현

---
+++ server/src/main/java/com/inha/server/clova/service/ClovaService.java
@@ -0,0 +1,80 @@
+package com.inha.server.clova.service;
+
+import com.inha.server.s3.service.S3Service;
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
+import java.util.Date;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+public class ClovaService {
+    @Value("${clova.api.client-id}")
+    String clientId;
+    @Value("${clova.api.client-secret}")
+    String clientSecret;
+    @Value("${clova.api.url}")
+    String apiURL;
+
+    private final S3Service s3Service;
+
+    public void executeTTS(String prompt) {
+        try {
+            String text = URLEncoder.encode(prompt, StandardCharsets.UTF_8);
+            URL url = new URL(apiURL);
+
+            HttpURLConnection con = (HttpURLConnection) url.openConnection();
+            con.setRequestMethod("POST");
+            con.setRequestProperty("X-NCP-APIGW-API-KEY-ID", clientId);
+            con.setRequestProperty("X-NCP-APIGW-API-KEY", clientSecret);
+
+            // post request
+            String postParams = "speaker=clara&volume=0&speed=0&pitch=0&format=mp3&text=" + text;
+            con.setDoOutput(true);
+            DataOutputStream wr = new DataOutputStream(con.getOutputStream());
+            wr.writeBytes(postParams);
+            wr.flush();
+            wr.close();
+            int responseCode = con.getResponseCode();
+            BufferedReader br;
+
+            if(responseCode==200) { // 정상 호출
+                InputStream is = con.getInputStream();
+                int read = 0;
+                byte[] bytes = new byte[1024];
+                // 랜덤한 이름으로 mp3 파일 생성
+                String tempname = Long.valueOf(new Date().getTime()).toString();
+                File f = new File(tempname + ".mp3");
+                f.createNewFile();
+                OutputStream outputStream = new FileOutputStream(f);
+                while ((read =is.read(bytes)) != -1) {
+                    outputStream.write(bytes, 0, read);
+                }
+                is.close();
+                s3Service.putS3(f, "clova" + tempname);
+            } else {  // 오류 발생
+                br = new BufferedReader(new InputStreamReader(con.getErrorStream()));
+                String inputLine;
+                StringBuffer response = new StringBuffer();
+                while ((inputLine = br.readLine()) != null) {
+                    response.append(inputLine);
+                }
+                br.close();
+                System.out.println(response.toString());
+            }
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+    }
+}
