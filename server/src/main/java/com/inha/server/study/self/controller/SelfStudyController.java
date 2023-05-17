31bbe94ae4874df42ec45a762d4a0853516d569e
Cho Ah Young <like0998904@gmail.com>
2023-05-17 13:17:00 +0900

    :art: feat : 셀프 스터디 생성 MockAPI 응답 형태 변경

--- server/src/main/java/com/inha/server/study/self/controller/SelfStudyController.java
+++ server/src/main/java/com/inha/server/study/self/controller/SelfStudyController.java
@@ -6,7 +6,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
 import java.util.LinkedHashMap;
+import java.util.List;
 import java.util.Map;
 
 @RestController
@@ -15,14 +17,13 @@ public class SelfStudyController {
 
     @GetMapping()
     public ScriptDto GetScript(@RequestParam(name = "languageId") Integer languageId, @RequestParam(name = "type") String type) {
-        Map<String, String> scripts = new LinkedHashMap<>();
-
-        scripts.put("Hello student.", "https://inha-talktalk.s3.ap-northeast-2.amazonaws.com/clova1684048155309");
-        scripts.put("I wanna go home.", "https://inha-talktalk.s3.ap-northeast-2.amazonaws.com/clova1684048259773");
-        scripts.put("Did you done homework?", "https://inha-talktalk.s3.ap-northeast-2.amazonaws.com/clova1684285913617");
-        scripts.put("I will do tomorrow.", "https://inha-talktalk.s3.ap-northeast-2.amazonaws.com/clova1684285998862");
-        scripts.put("See you next week.", "https://inha-talktalk.s3.ap-northeast-2.amazonaws.com/clova1684286341266");
-        scripts.put("Bye teacher.", "https://inha-talktalk.s3.ap-northeast-2.amazonaws.com/clova1684286376009");
+        List<ScriptDto.Script> scripts = new ArrayList<>();
+        scripts.add(new ScriptDto.Script("Hello student.", "https://inha-talktalk.s3.ap-northeast-2.amazonaws.com/clova1684048155309"));
+        scripts.add(new ScriptDto.Script("I wanna go home.", "https://inha-talktalk.s3.ap-northeast-2.amazonaws.com/clova1684048259773"));
+        scripts.add(new ScriptDto.Script("Did you done homework?", "https://inha-talktalk.s3.ap-northeast-2.amazonaws.com/clova1684285913617"));
+        scripts.add(new ScriptDto.Script("I will do tomorrow.", "https://inha-talktalk.s3.ap-northeast-2.amazonaws.com/clova1684285998862"));
+        scripts.add(new ScriptDto.Script("See you next week.", "https://inha-talktalk.s3.ap-northeast-2.amazonaws.com/clova1684286341266"));
+        scripts.add(new ScriptDto.Script("Bye teacher.", "https://inha-talktalk.s3.ap-northeast-2.amazonaws.com/clova1684286376009"));
 
         return ScriptDto.builder()
                 .scriptId("dfjksdjflak32iji2")
--- server/src/main/java/com/inha/server/study/self/dto/reponse/ScriptDto.java
+++ server/src/main/java/com/inha/server/study/self/dto/reponse/ScriptDto.java
@@ -1,13 +1,22 @@
 package com.inha.server.study.self.dto.reponse;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 
+import java.util.List;
 import java.util.Map;
 
 @Getter
 @Builder
 public class ScriptDto {
     private String scriptId;
-    private Map<String, String> scripts;
+    private List<Script> scripts;
+
+    @AllArgsConstructor
+    @Getter
+    public static class Script {
+        private String text;
+        private String mp3Uri;
+    }
 }
