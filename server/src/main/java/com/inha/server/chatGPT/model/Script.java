1cd64c0e64d5ce0038645bdd5679b850a4652236
Cho Ah Young <like0998904@gmail.com>
2023-05-18 16:09:30 +0900

    :card_file_box: Script Document 생성

---
+++ server/src/main/java/com/inha/server/chatGPT/model/Script.java
@@ -0,0 +1,29 @@
+package com.inha.server.chatGPT.model;
+
+import java.util.List;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Builder
+@Getter
+@Document("script")
+public class Script {
+
+    @Id
+    String id;
+
+    String type;
+
+    List<ScriptMap> scripts;
+
+    @Getter
+    @AllArgsConstructor
+    public static class ScriptMap {
+
+        String text;
+        String mp3Uri;
+    }
+}
