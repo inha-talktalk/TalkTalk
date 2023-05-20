0751dc78e36754661f9b3a162bed7f112eb3cd4b
Cho Ah Young <like0998904@gmail.com>
2023-05-20 14:48:32 +0900

    :zap: refactor: ScriptDto 코드 리팩토링

--- server/src/main/java/com/inha/server/study/self/dto/reponse/ScriptDto.java
+++ server/src/main/java/com/inha/server/study/self/dto/reponse/ScriptDto.java
@@ -1,22 +1,14 @@
 package com.inha.server.study.self.dto.reponse;
 
-import lombok.AllArgsConstructor;
+import com.inha.server.chatGPT.model.Script.ScriptMap;
+import java.util.List;
 import lombok.Builder;
 import lombok.Getter;
-
-import java.util.List;
-import java.util.Map;
 
 @Getter
 @Builder
 public class ScriptDto {
-    private String scriptId;
-    private List<Script> scripts;
 
-    @AllArgsConstructor
-    @Getter
-    public static class Script {
-        private String text;
-        private String mp3Uri;
-    }
+    private String scriptId;
+    private List<ScriptMap> scripts;
 }
