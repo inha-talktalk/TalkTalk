b1aa8f81e41b58d09e4469071767cdb03857b378
jiwon1217 <koagjw@gmail.com>
2023-05-25 17:11:44 +0900

    :necktie: feat: 일반 채팅 전송 request

---
+++ server/src/main/java/com/inha/server/study/group/dto/request/PostGeneralChatReq.java
@@ -0,0 +1,13 @@
+package com.inha.server.study.group.dto.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class PostGeneralChatReq {
+
+    private String message;
+}
