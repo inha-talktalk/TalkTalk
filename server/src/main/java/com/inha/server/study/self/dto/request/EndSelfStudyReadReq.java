1144561b5805ca57fd208db20863f0f39acdc18e
Cho Ah Young <like0998904@gmail.com>
2023-05-27 16:47:58 +0900

    :sparkles: feat: POST /self-study/read RequestDto 형태 수정

--- server/src/main/java/com/inha/server/study/self/dto/request/EndSelfStudyReadReq.java
+++ server/src/main/java/com/inha/server/study/self/dto/request/EndSelfStudyReadReq.java
@@ -1,16 +1,19 @@
 package com.inha.server.study.self.dto.request;
 
-import com.inha.server.chatGPT.model.Script.ScriptMap;
+import java.util.List;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-
-import java.util.List;
+import lombok.Setter;
+import org.springframework.web.multipart.MultipartFile;
 
 @Getter
+@Setter
 @NoArgsConstructor
 @AllArgsConstructor
 public class EndSelfStudyReadReq {
+
+    List<MultipartFile> fileList;
     private String selfStudyId;
-    private List<ScriptMap> answers;
+    private String textList;
 }
