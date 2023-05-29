e846a210f0a5b5cc61b7719fefd4c7724fbbd714
dolphinlmg <dolphinlmg@naver.com>
2023-05-29 16:44:55 +0900

    ✨ feat: 셀프 스터디 읽기 기능 추가

--- client/components/SelfStudyPanel/SelfStudyPanelRead/index.tsx
+++ client/components/SelfStudyPanel/SelfStudyPanelRead/index.tsx
@@ -1,9 +1,16 @@
-import { useState } from 'react';
 import { RiRecordCircleFill } from 'react-icons/ri';
 import { useGlobalTheme } from '@/styles/GlobalThemeContext';
 import ChatBubble from '@/components/ChatBubble';
 import { style } from '../style';
 import ChatInputBar from '@/components/ChatInputBar';
+import { useSpeech } from '@/utils/speechRecognition';
+import { useRecoilState, useRecoilValue } from 'recoil';
+import { selfStudy, selfStudyStarted, submitSelfStudy } from '@/states/selfStudy';
+import { useEffect, useState } from 'react';
+import { useRouter } from 'next/router';
+import useRecord from '@/utils/record';
+import { postSelfStudyRead } from '@/utils/api';
+import { toast } from 'react-toastify';
 
 interface SelfStudyPanelReadProps {
   script: SelfStudyScriptResponse | null;
@@ -11,45 +18,87 @@ interface SelfStudyPanelReadProps {
 
 export default function SelfStudyPanelRead({ script }: SelfStudyPanelReadProps) {
   const { theme } = useGlobalTheme();
-  const [chatValue, setChatValue] = useState<string>('');
+  const [myChatList, setMyChatList] = useState<string[]>([]);
+  const [recordFileList, setRecordFileList] = useState<File[]>([]);
+  const selfStudyInfo = useRecoilValue(selfStudy);
+  const { value, start, stop, isListening, resetValue } = useSpeech(selfStudyInfo?.languaegStt);
+  const isSelfStudyStarted = useRecoilValue(selfStudyStarted);
+  const [submit, setSubmit] = useRecoilState(submitSelfStudy);
+  const router = useRouter();
+  const { startRecord, stopRecord } = useRecord();
+
+  useEffect(() => {
+    if (!router.isReady) return;
+
+    if (submit) {
+      setSubmit(false);
+      if (!script) return;
+      if (myChatList.length !== script.scripts.length) {
+        toast.warn('스터디를 모두 완료해 주세요');
+        return;
+      }
+      (async () => {
+        if (!selfStudyInfo || selfStudyInfo.selfStudyId === null) return;
+        const form = new FormData();
+        recordFileList.forEach((file) => form.append('fileList', file));
+        form.append('textList', JSON.stringify(myChatList));
+        form.append('selfStudyId', selfStudyInfo.selfStudyId);
+
+        await postSelfStudyRead(form);
+      })();
+    }
+  }, [myChatList, recordFileList, router.isReady, script, selfStudyInfo, setSubmit, submit]);
+
+  if (!script) return <></>;
 
   return (
     <div css={style.container}>
       <div css={style.bubbleContainer}>
-        <div className="left">
-          <ChatBubble position={'left'} value={'Hey! Long time no see, how have you been?'} />
-        </div>
-        <div className="right">
-          <ChatBubble position={'right'} value={'Hey! Long time no see, how have you been?'} />
-        </div>
-        <div className="left">
-          <ChatBubble
-            position={'left'}
-            value={`Hey, yeah it's been ages! I've been good, thanks for asking. How about you?`}
-          />
-        </div>
-        <div className="right">
-          <ChatBubble
-            position={'right'}
-            value={`Hey, yeah it's been ages! I've been good, thanks for asking. How about you?`}
-          />
-        </div>
-        <div className="left">
-          <ChatBubble
-            position={'left'}
-            value={`Hey, yeah it's been ages! I've been good, thanks for asking. How about you?`}
-          />
-        </div>
-        <div className="right">
-          <ChatBubble
-            position={'right'}
-            value={`Hey, yeah it's been ages! I've been good, thanks for asking. How about you?`}
-          />
-        </div>
+        {Array(Math.min(myChatList.length + 1, script.scripts.length))
+          .fill(0)
+          .map((_, idx) => (
+            <>
+              <div className="left">
+                <ChatBubble position={'left'} value={script.scripts[idx].text} />
+              </div>
+              <div className="right">
+                <ChatBubble position={'right'} value={myChatList[idx] ?? '...'} />
+              </div>
+            </>
+          ))}
       </div>
       <div css={style.chatInputContainer}>
-        <ChatInputBar value={chatValue} onSend={() => {}} onChange={setChatValue}>
-          <RiRecordCircleFill size={36} color={theme.secondary} />
+        <ChatInputBar
+          value={value}
+          onSend={() => {
+            setMyChatList((list) => [...list, value]);
+            resetValue();
+          }}
+          disabled={!isSelfStudyStarted}
+        >
+          <RiRecordCircleFill
+            size={36}
+            color={isListening ? '#F56060' : theme.secondary}
+            onClick={() => {
+              if (!isSelfStudyStarted) return;
+              if (isListening) {
+                stop();
+                stopRecord(`${myChatList.length + 1}.mp3`).then((value) => {
+                  setRecordFileList((list) => {
+                    if (list.length === myChatList.length) {
+                      return [...list, value];
+                    } else {
+                      return list.map((file, idx) => (idx === list.length - 1 ? value : file));
+                    }
+                  });
+                });
+              } else {
+                start();
+                startRecord();
+                resetValue();
+              }
+            }}
+          />
         </ChatInputBar>
       </div>
     </div>
