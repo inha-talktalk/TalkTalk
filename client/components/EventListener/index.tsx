8c21561c78df97c62fac2fdf55eb98ccb427fd21
dolphinlmg <dolphinlmg@naver.com>
2023-05-30 17:44:45 +0900

    🐛 fix: axios instance에도 헤더 제거

--- client/components/EventListener/index.tsx
+++ client/components/EventListener/index.tsx
@@ -1,4 +1,5 @@
 import { jwtState } from '@/states/login';
+import { apiAxiosInstance } from '@/utils/api';
 import { useRouter } from 'next/router';
 import { ReactNode, useEffect } from 'react';
 import { toast } from 'react-toastify';
@@ -17,6 +18,7 @@ export default function EventListener({ children }: EventListenerProps) {
 
     const handler = () => {
       localStorage.removeItem('jwt');
+      delete apiAxiosInstance.defaults.headers.common['x-access-token'];
       setJwt('');
       router.push('/');
       toast.info('로그인이 만료되었습니다.', { toastId: 'jwt' });
