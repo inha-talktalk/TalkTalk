174783f3f222ad9b4ac04f9e95dfd079e23a2e49
dolphinlmg <dolphinlmg@naver.com>
2023-06-01 14:21:35 +0900

    ✨ feat: OwnerSetting 컴포넌트 적용

--- client/components/GroupStudyController/GroupStudySetting/style.ts
+++ client/components/GroupStudyController/GroupStudySetting/style.ts
@@ -2,25 +2,13 @@ import { css } from '@emotion/react';
 
 export const style = {
   container: (backgroundColor: string) => css`
-    width: 550px;
-    height: 340px;
+    height: 400px;
     border-radius: 10px;
-    padding: 15px 90px;
+    padding: 15px 15px;
     box-sizing: border-box;
     background-color: ${backgroundColor};
-  `,
-  inner: css`
-    height: calc(100% - 70px);
     display: flex;
     flex-direction: column;
-    overflow-y: auto;
-    margin: 15px 0;
-
-    & > div {
-      display: flex;
-      align-items: center;
-      padding: 0 15px;
-      box-sizing: border-box;
-    }
+    justify-content: space-between;
   `,
 };
