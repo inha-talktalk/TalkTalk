e9f640e598ff063ea7f8b589350ec7ee980de484
dolphinlmg <dolphinlmg@naver.com>
2023-06-08 17:09:45 +0900

    ✨ feat: 쓰기 완료 페이지 음성 기능 추가

--- client/components/SelfStudyPanel/SelfStudyPanelWriteDone/index.tsx
+++ client/components/SelfStudyPanel/SelfStudyPanelWriteDone/index.tsx
@@ -1,9 +1,10 @@
-import { useState } from 'react';
+import React, { useCallback, useState } from 'react';
 import { useGlobalTheme } from '@/styles/GlobalThemeContext';
 import { AiFillPlayCircle } from 'react-icons/ai';
 import ChatBubble from '@/components/ChatBubble';
 import { style } from '../style';
 import ChatInputBar from '@/components/ChatInputBar';
+import useAudio from '@/utils/audio';
 
 interface SelfStudyPanelWriteDoneProps {
   script: SelfStudyScriptResponse | null;
@@ -13,6 +14,9 @@ interface SelfStudyPanelWriteDoneProps {
 export default function SelfStudyPanelWriteDone({ script, answers }: SelfStudyPanelWriteDoneProps) {
   const { theme } = useGlobalTheme();
   const [chatValue, setChatValue] = useState<string>('');
+  const [url, setUrl] = useState<string>('');
+
+  const { startPlaying, stopPlaying } = useAudio(url);
 
   if (!script) return <></>;
 
@@ -22,14 +26,24 @@ export default function SelfStudyPanelWriteDone({ script, answers }: SelfStudyPa
         {Array(script?.scripts.length)
           .fill(0)
           .map((_, idx) => (
-            <>
+            <React.Fragment key={idx}>
               <div className="left">
-                <ChatBubble position={'left'} value={script.scripts[idx].text} />
+                <ChatBubble
+                  position={'left'}
+                  value={script.scripts[idx].text}
+                  onClick={() => {
+                    stopPlaying();
+                    setUrl(script.scripts[idx].mp3Uri);
+                    setTimeout(() => {
+                      startPlaying();
+                    }, 1);
+                  }}
+                />
               </div>
               <div className="right">
                 <ChatBubble position={'right'} value={answers[idx]} />
               </div>
-            </>
+            </React.Fragment>
           ))}
       </div>
       <div css={style.chatInputContainer}>
